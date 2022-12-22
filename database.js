bff16bd115fef6a0cc364d6ca587167083329f41
Kenny Gonsalves <gonsalveskenny@gmail.com>
2022-12-22 11:23:55 +0000

    Initial Commit

---
+++ database.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json())
+
+const connection = mysql.createConnection({
+  host: '52.226.222.222',
+  user: 'root',
+  password: 'kenny',
+  database: 'notes_app'
+});
+
+app.get('/notes', (req, res) => {
+  connection.query('SELECT * FROM notes', (error, results) => {
+    if (error) {
+      return res.send(error);
+    }
+    res.send(results);
+  });
+});
+
+app.post('/notes', (req, res) => {
+  const { title, contents } = req.body;
+  connection.query('INSERT INTO notes (title, contents) VALUES (?, ?)', [title, contents], (error, results) => {
+    if (error) {
+      return res.send(error);
+    }
+    res.send({ message: 'Note created successfully!' });
+  });
+});
+
+app.get('/notes/:id', (req, res) => {
+  const id = req.params.id;
+  connection.query('SELECT * FROM notes WHERE id = ?', [id], (error, results) => {
+    if (error) {
+      return res.send(error);
+    }
+    res.send(results[0]);
+  });
+});
+
+app.put('/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const { title, contents } = req.body;
+  connection.query(
+    'UPDATE notes SET title = ?, contents = ? WHERE id = ?',
+    [title, contents, id],
+    (error, results) => {
+      if (error) {
+        return res.send(error);
+      }
+      res.send({ message: 'Note updated successfully!' });
+    }
+  );
+});
+
+app.delete('/notes/:id', (req, res) => {
+  const id = req.params.id;
+  connection.query('DELETE FROM notes WHERE id = ?', [id], (error, results) => {
+    if (error) {
+      return res.send(error);
+    }
+    res.send({ message: 'Note deleted successfully!' });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`API listening on port ${port}`);
+});
