4edffca65628bb61da44fadb9f66b1f1dace1b67
Kenny Gonsalves <gonsalveskenny@gmail.com>
2022-12-23 22:23:17 +0530

    Modified

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const jwt = require('jsonwebtoken');
+const { process } = require('ipaddr.js');
 
 const app = express();
 const port = 3000;
@@ -8,11 +9,11 @@ const port = 3000;
 app.use(express.json())
 
 const connection = mysql.createConnection({
-  host: 'containers-us-west-60.railway.app',
-  user: 'root',
-  password: 'VzElDcFqdvoBXH5jnpR9',
-  port: '5860',
-  database: 'railway'
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+  database: process.env.DATABASE
 });
 
 app.post('/login', (req, res) => {
