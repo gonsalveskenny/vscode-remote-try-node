fc45c5a9d6e8d5b28cf2e81e2a0463a4d3699f7f
Kenny Gonsalves <gonsalveskenny@gmail.com>
2022-12-23 17:39:03 +0000

    Modified

--- database.js
+++ database.js
@@ -2,17 +2,19 @@ const express = require('express');
 const mysql = require('mysql2');
 const jwt = require('jsonwebtoken');
 
+require('dotenv').config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(express.json())
 
 const connection = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
-  database: process.env.DATABASE
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  port: process.env.MYSQL_PORT,
+  database: process.env.MYSQL_DATABASE
 });
 
 app.post('/login', (req, res) => {
